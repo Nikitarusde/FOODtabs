4eac2566b4273ea5f62c309782d0c8342cf9180a
Nikita <nikistosov@gmail.com>
2021-12-05 18:58:01 +0100

    tabs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
     
 });
